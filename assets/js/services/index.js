5eec9e07f1de8f624e064e3a8943a6ba0dbeddc2
Fabio Dias <fabiowebmain@gmail.com>
2019-08-03 23:35:20 -0300

    exclusão de arquivo

--- assets/js/services/index.js
+++ assets/js/services/index.js
@@ -1,11 +1,9 @@
 'use strict';
-import CheckoutService from './CheckoutService';
 import Confirm from './Confirm';
 import Enums from './Enums';
 
 require('angular')
     .module('recruitmentTest')
-    .service('CheckoutService', CheckoutService)
     .service('Confirm', Confirm)
     .service('Enums', Enums);
 
